6c01caaa58892907c8643696d3878358d653303e
Av-code80 <dev.avehghasemian@gmail.com>
2024-04-15 18:32:27 +0200

    feat: :sparkles: DeleteEmployee logic implementation in component with Improuved UL

--- src/app/employee/[employeeId]/edit/page.tsx
+++ src/app/employee/[employeeId]/edit/page.tsx
@@ -5,14 +5,94 @@
  */
 "use client";
 import React, { useState, useEffect } from "react";
+import { useUpdateEmployee } from "@/domain/hooks/useUpdateEmployee.hook";
+import { useParams, useRouter } from "next/navigation";
+import { useGetEmployeeById } from "@/domain/hooks/useGetEmployeeById.hook";
+import { UpdateEmployeeParams } from "@/domain/models/employee.model";
 
 export default function EditEmployeePage() {
+  const params = useParams();
+  const employeeId = params.employeeId as string;
+  const employeeIdNum = parseInt(employeeId, 10);
+  const router = useRouter();
+
+  // Fetch the employee details for editing
+  const {
+    data: employee,
+    isLoading: isFetching,
+    isError: isFetchError,
+  } = useGetEmployeeById(employeeIdNum);
+
+  // Prepare the form state with the fetched employee data
+  // State initialization without the `id`
+  const [form, setForm] = useState<Omit<UpdateEmployeeParams, "id">>({
+    name: "",
+    salary: "",
+    age: "",
+  });
+
+  // Use the form fields without the `id` for the editable form fields
+
+  useEffect(() => {
+    if (employee) {
+      setForm({
+        name: employee.employee_name,
+        salary: employee.employee_salary.toString(),
+        age: employee.employee_age.toString(),
+      });
+    }
+  }, [employee]);
+
+  const { mutate: updateEmployee } = useUpdateEmployee();
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    updateEmployee(
+      {
+        id: employeeIdNum,
+        ...form,
+      },
+      {
+        onSuccess: () => {
+          router.push("/employees");
+        },
+      }
+    );
+  };
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setForm((prev) => ({ ...prev, [name]: value }));
+  };
+
+  if (isFetching)
+    return (
+      <div className="flex h-screen justify-center items-center">
+        <div className="text-lg text-white font-bold bg-clip-text text-transparent bg-gradient-to-r from-custom-pink to-custom-orange animate-pulse">
+          Loading ⚙...
+        </div>
+      </div>
+    );
+
+  if (isFetchError)
+    return (
+      <div className="flex h-screen justify-center items-center p-4">
+        <div className="text-lg font-bold text-white bg-red-500 rounded-lg p-3 shadow-lg animate-pulse">
+          Error fetching the employee data
+        </div>
+      </div>
+    );
+
   return (
     <main className="flex flex-col h-screen items-center p-4 justify-center gap-10 bg-gradient-to-r from-pink-200 via-purple-200 to-blue-200">
       <h1 className="text-2xl font-bold text-sky-800 text-center">
         Edit Employee
       </h1>
-      <form className="space-y-4 w-1/2" aria-label="Edit Employee Form">
+      <form
+        onSubmit={handleSubmit}
+        className="space-y-4 w-1/2"
+        aria-label="Edit Employee Form"
+      >
         <div className="form-group">
           <label
             htmlFor="name"
@@ -23,7 +103,9 @@ export default function EditEmployeePage() {
           <input
             type="text"
             name="name"
-            className="mt-1 block w-full "
+            value={form.name}
+            onChange={handleChange}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             aria-required="true"
           />
         </div>
@@ -37,7 +119,9 @@ export default function EditEmployeePage() {
           <input
             type="text"
             name="salary"
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md "
+            value={form.salary}
+            onChange={handleChange}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             aria-required="true"
           />
         </div>
@@ -51,13 +135,15 @@ export default function EditEmployeePage() {
           <input
             type="text"
             name="age"
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md "
+            value={form.age}
+            onChange={handleChange}
+            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
             aria-required="true"
           />
         </div>
         <button
           type="submit"
-          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md "
+          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 hover:bg-gradient-to-r hover:from-pink-600  hover:via-purple-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
         >
           Update
         </button>
--- src/app/employee/[employeeId]/page.tsx
+++ src/app/employee/[employeeId]/page.tsx
@@ -1,151 +1,89 @@
 /**
- * EditEmployeePage Component
- * @description A form page for editing an existing employee's details. It fetches employee data based on the ID from the URL and allows updating their name, salary, and age.
- * @returns {React.Component} The React component for editing an employee.
+ * DetailsEmployeePage Component
+ * @description Displays details for a specific employee and allows for operations like editing and deleting that employee.
+ * @returns {React.Component} The React component displaying detailed employee information.
  */
-
 "use client";
-import React, { useState, useEffect } from "react";
-import { useUpdateEmployee } from "@/domain/hooks/useUpdateEmployee.hook";
-import { useParams, useRouter } from "next/navigation";
+import React from "react";
 import { useGetEmployeeById } from "@/domain/hooks/useGetEmployeeById.hook";
-import { UpdateEmployeeParams } from "@/domain/models/employee.model";
+import EmployeeCard from "@/ui/components/EmployeeCard.component";
+import Link from "next/link";
+import { useParams, useRouter } from "next/navigation";
+import { useDeleteEmployee } from "@/domain/hooks/useDeleteEmployeeById.hook";
 
-export default function EditEmployeePage() {
-  const params = useParams();
-  const employeeId = params.employeeId as string;
-  const employeeIdNum = parseInt(employeeId, 10);
+export default function DetailsEmployeePage() {
   const router = useRouter();
+  const params = useParams();
+  const employeeId = params.employeeId as string; // Ensuring type safety.
+  const employeeIdNum = parseInt(employeeId, 10);
 
   const {
     data: employee,
-    isLoading: isFetching,
-    isError: isFetchError,
+    isLoading,
+    isError,
   } = useGetEmployeeById(employeeIdNum);
+  const { mutate: deleteEmployee } = useDeleteEmployee();
 
-  // State initialization without the `id`
-  const [form, setForm] = useState<Omit<UpdateEmployeeParams, "id">>({
-    name: "",
-    salary: "",
-    age: "",
-  });
-
-  // Use the form fields without the `id` for the editable form fields
-  useEffect(() => {
-    if (employee) {
-      setForm({
-        name: employee.employee_name,
-        salary: employee.employee_salary.toString(),
-        age: employee.employee_age.toString(),
+  const handleDelete = () => {
+    if (employeeIdNum) {
+      deleteEmployee(employeeIdNum, {
+        onSuccess: () => router.push("/employees"),
+        onError: (error) => {
+          alert(`Error deleting employee: ${error.message}`);
+        },
       });
     }
-  }, [employee]);
-
-  const { mutate: updateEmployee } = useUpdateEmployee();
-
-  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault();
-    updateEmployee(
-      {
-        id: employeeIdNum,
-        ...form,
-      },
-      {
-        onSuccess: () => {
-          router.push("/employees");
-        },
-      }
-    );
   };
 
-  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const { name, value } = e.target;
-    setForm((prev) => ({ ...prev, [name]: value }));
-  };
-
-  if (isFetching)
+  if (isLoading) {
     return (
       <div className="flex h-screen justify-center items-center">
-        <div className="text-lg text-white font-bold bg-clip-text text-transparent bg-gradient-to-r from-custom-pink to-custom-orange animate-pulse">
-          Loading ⚙...
+        <div
+          role="alert"
+          aria-busy="true"
+          className="text-lg font-bold text-white bg-gradient-to-r from-purple-500 to-pink-500 rounded-lg p-3 animate-pulse"
+        >
+          Loading...
         </div>
       </div>
     );
+  }
 
-  if (isFetchError)
+  if (!employee && isError) {
     return (
-      <div className="flex h-screen justify-center items-center p-4">
-        <div className="text-lg font-bold text-white bg-red-500 rounded-lg p-3 shadow-lg animate-pulse">
-          Error fetching the employee data
+      <div className="flex h-screen justify-center items-center">
+        <div
+          role="alert"
+          className="text-lg font-bold text-white bg-red-500 rounded-lg p-3 shadow-lg animate-pulse"
+        >
+          An error occurred while fetching the employee details.
         </div>
       </div>
     );
+  }
 
   return (
-    <main className="flex flex-col h-screen items-center p-4 justify-center gap-10 bg-gradient-to-r from-pink-200 via-purple-200 to-blue-200">
-      <h1 className="text-2xl font-bold text-sky-800 text-center">
-        Edit Employee
-      </h1>
-      <form
-        onSubmit={handleSubmit}
-        className="space-y-4 w-1/2"
-        aria-label="Edit Employee Form"
-      >
-        <div className="form-group">
-          <label
-            htmlFor="name"
-            className="block text-sm font-medium text-neutral-600"
+    <main className="flex flex-col h-screen items-center justify-center gap-20 bg-gradient-to-r from-pink-200 via-purple-200 to-blue-200">
+      <h1 className="text-2xl font-bold text-sky-800">Employee Details</h1>
+      {employee && <EmployeeCard employee={employee} />}
+      {employee && (
+        <div className="flex space-x-4">
+          <Link
+            href={`/employee/${employee?.id}/edit`}
+            className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-green-400 to-blue-500 hover:from-green-500 hover:to-blue-600"
+            aria-label="Edit employee"
           >
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            value={form.name}
-            onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            aria-required="true"
-          />
-        </div>
-        <div className="form-group">
-          <label
-            htmlFor="salary"
-            className="block text-sm font-medium text-neutral-600"
+            ✏️ Edit
+          </Link>
+          <button
+            onClick={handleDelete}
+            className="inline-flex items-center justify-center px-4 py-2 border border-transparent text-sm font-medium rounded-md shadow-sm text-white bg-gradient-to-r from-red-400 to-red-500 hover:from-red-500 hover:to-red-700"
+            aria-label="Delete employee"
           >
-            Salary
-          </label>
-          <input
-            type="text"
-            name="salary"
-            value={form.salary}
-            onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            aria-required="true"
-          />
+            🗑️ Delete
+          </button>
         </div>
-        <div className="form-group">
-          <label
-            htmlFor="age"
-            className="block text-sm font-medium text-neutral-600"
-          >
-            Age
-          </label>
-          <input
-            type="text"
-            name="age"
-            value={form.age}
-            onChange={handleChange}
-            className="mt-1 block w-full px-3 py-2 border border-gray-300 rounded-md shadow-sm focus:outline-none focus:ring-indigo-500 focus:border-indigo-500"
-            aria-required="true"
-          />
-        </div>
-        <button
-          type="submit"
-          className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-gradient-to-r from-pink-500 via-purple-500 to-blue-500 hover:bg-gradient-to-r hover:from-pink-600  hover:via-purple-600 hover:to-blue-600 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-        >
-          Update
-        </button>
-      </form>
+      )}
     </main>
   );
 }
